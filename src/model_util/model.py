804a69fb14a0dfaddf13250faa267d822ac501a1
Netra-Patel <netra.patel@ucalgary.ca>
2021-06-10 13:42:19 -0500

    updates

--- src/model_util/model.py
+++ src/model_util/model.py
@@ -6,7 +6,7 @@ import confuse
 from pathlib import Path
 import torch
 import numpy as np
-from dataset_util.processor import MarketDataset
+from processor import MarketDataset
 
 config = confuse.Configuration('market1501', __name__)
 config.set_file(Path(
@@ -16,17 +16,15 @@ test_obj = MarketDataset(
 train_obj = MarketDataset(
     config['market_1501_ds']['train_path'].get(), True, True)
 
-for i in range(len(test_obj.paths)):
-    test_obj.view_sample(i)
-for i in range(len(train_obj.paths)):
-    train_obj.view_sample(i)
-#trainimg = np.true_divide(trainimg, 255)
-#backbone = resnet_fpn_backbone('resnet50', pretrained=True, trainable_layers=3)
-#trainimg_np = torch.from_numpy(trainimg).type('torch.DoubleTensor')
-#trainimg_np = torch.as_tensor(trainimg_np).type('torch.DoubleTensor')
-#trainimg_np = trainimg_np.unsqueeze(3)
-#trainimg_np = torch.reshape(trainimg_np, (64, 3, 128, 1))
-#trainimg_np = trainimg_np.to(torch.double)
-#out = backbone(trainimg_np.float())
-# print(trainimg_np.shape)
-#print([(k, v.shape) for k, v in out.items()])
+testimg, testattr = test_obj.view_sample(14560)
+trainimg, trainattr = train_obj.view_sample(10560)
+trainimg = np.true_divide(trainimg, 255)
+backbone = resnet_fpn_backbone('resnet50', pretrained=True, trainable_layers=3)
+trainimg_np = torch.from_numpy(trainimg).type('torch.DoubleTensor')
+trainimg_np = torch.as_tensor(trainimg_np).type('torch.DoubleTensor')
+trainimg_np = trainimg_np.unsqueeze(3)
+trainimg_np = torch.reshape(trainimg_np, (64, 3, 128, 1))
+trainimg_np = trainimg_np.to(torch.double)
+out = backbone(trainimg_np.float())
+print(trainimg_np.shape)
+print([(k, v.shape) for k, v in out.items()])
