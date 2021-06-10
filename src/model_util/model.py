c0f13d21f572eae7da9a4ada56a0eed29cf5f3fa
Netra-Patel <netra.patel@ucalgary.ca>
2021-06-10 16:57:02 -0500

    Update model.py

--- src/model_util/model.py
+++ src/model_util/model.py
@@ -8,23 +8,27 @@ import torch
 import numpy as np
 from processor import MarketDataset
 
-config = confuse.Configuration('market1501', __name__)
-config.set_file(Path(
-    r"C:\\Users\\netra\\GithubEncm369\\reid\\explainable-id-reid\\src\\dataset_util\\market1501.yml"))
-test_obj = MarketDataset(
-    config['market_1501_ds']['test_path'].get(), True, False)
-train_obj = MarketDataset(
-    config['market_1501_ds']['train_path'].get(), True, True)
 
-testimg, testattr = test_obj.view_sample(14560)
-trainimg, trainattr = train_obj.view_sample(10560)
-trainimg = np.true_divide(trainimg, 255)
-backbone = resnet_fpn_backbone('resnet50', pretrained=True, trainable_layers=3)
-trainimg_np = torch.from_numpy(trainimg).type('torch.DoubleTensor')
-trainimg_np = torch.as_tensor(trainimg_np).type('torch.DoubleTensor')
-trainimg_np = trainimg_np.unsqueeze(3)
-trainimg_np = torch.reshape(trainimg_np, (64, 3, 128, 1))
-trainimg_np = trainimg_np.to(torch.double)
-out = backbone(trainimg_np.float())
-print(trainimg_np.shape)
-print([(k, v.shape) for k, v in out.items()])
+if __name__ == "__main__":
+    config = confuse.Configuration('market1501', __name__)
+    config.set_file(Path(
+        r"C:\\Users\\netra\\GithubEncm369\\reid\\explainable-id-reid\\src\\dataset_util\\market1501.yml"))
+    from dataset_util.processor import MarketDataset
+    test_obj = MarketDataset(
+        config['market_1501_ds']['test_path'].get(), True, False)
+    train_obj = MarketDataset(
+        config['market_1501_ds']['train_path'].get(), True, True)
+
+    testimg, testattr = test_obj.view_sample(14560)
+    trainimg, trainattr = train_obj.view_sample(10560)
+    trainimg = np.true_divide(trainimg, 255)
+    backbone = resnet_fpn_backbone(
+        'resnet50', pretrained=True, trainable_layers=3)
+    trainimg_np = torch.from_numpy(trainimg).type('torch.DoubleTensor')
+    trainimg_np = torch.as_tensor(trainimg_np).type('torch.DoubleTensor')
+    trainimg_np = trainimg_np.unsqueeze(3)
+    trainimg_np = torch.reshape(trainimg_np, (64, 3, 128, 1))
+    trainimg_np = trainimg_np.to(torch.double)
+    out = backbone(trainimg_np.float())
+    print(trainimg_np.shape)
+    print([(k, v.shape) for k, v in out.items()])
