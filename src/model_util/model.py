0c9d3cd1a1beec224f58ce2324b5e4e7695e39dc
DG-20 <div4312@gmail.com>
2021-06-24 15:07:17 -0600

    Cleaned up code

--- src/model_util/model.py
+++ src/model_util/model.py
@@ -20,7 +20,7 @@ class OverallModel(nn.Module):
         back_out = self.backbone(input)
         output = self.classifier(back_out, self.output_to_use)
 
-        return output, targets
+        return output
 
 class Classifier(nn.Module):
     # configurable, default activation layer is ReLU, but can be changed. Default setting for dropout
@@ -77,6 +77,24 @@ class Classifier(nn.Module):
 def collate_fn(batch):
     return tuple(zip(*batch))
 
+def training_loop(torch_ds, optimizer, device, model, loss, epochs = 20):
+    for i in range(epochs):
+        for data in iter(torch_ds):
+            # get the inputs; data is a list of [inputs, labels]
+            inputs, labels = data
+            optimizer.zero_grad()
+            # This doesn't work because inputs is a tuple.
+            images = list(image.to(device) for image in inputs)
+            targets = [{k: v.to(device) for k, v in t.items()} for t in labels]
+            output = model.forward(images[0], targets[0])
+            loss = criteria(output, targets[0])
+            loss.backward()
+            optimizer.step()
+        torch.save({'model': obj.state_dict(),
+                    'optimizer': optimizer.state_dict()
+                    }, str(i).zfill(3) + "resnet50_fpn_frcnn_full.tar")
+
+
 if __name__ == "__main__":
     config = confuse.Configuration('market1501', __name__)
     config.set_file(Path(r"C:\\Users\\Div\\Desktop\\Research\\reid\\reid\\explainable-id-reid\\src\\dataset_util\\market1501.yml"))
@@ -88,8 +106,6 @@ if __name__ == "__main__":
     validate_obj = MarketDataset(
         config['market_1501_ds']['train_path'].get(), True, 1)
 
-    bad = 0
-    good = 0
     torch_ds_test = torch.utils.data.DataLoader(test_obj,
                                            batch_size=2, num_workers=8,
                                            collate_fn=collate_fn)
@@ -105,16 +121,10 @@ if __name__ == "__main__":
     train_data = iter(torch_ds_train)
     print(f"Count of train: {len(train_data)}")
     validate_data = iter(torch_ds_val)
-    imgs = []
-    attrs = []
-    for img, attr in train_data:
-        imgs.append(img)
-        attrs.append(attr)
-        break
     print(f"Count of validate: {len(validate_data)}")
     #print(imgs[1][1].shape)
 
-    ####TESTING HERE
+    # Parameters for loop:
     backbone = resnet_fpn_backbone('resnet50', pretrained=True, trainable_layers=0)
     cfg = confuse.Configuration('model_architecture', __name__, read= False)
     cfg.set_file("C:\\Users\\Div\\Desktop\\Research\\reid\\reid\\explainable-id-reid\\src\\model_util\\classifier_architecture.yml")
@@ -122,59 +132,36 @@ if __name__ == "__main__":
     # The second argument is the output being used as a String,
     # "1", "2", "3", or "pool"
     obj = Classifier(classifier_params, "3")
-
     model = OverallModel(backbone, obj, "3")
-
-    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
-
-    print(imgs[0][0].shape)
-    out = model.forward(imgs[0], attrs[0])
-    print(out)
     criteria = nn.CrossEntropyLoss()
-    # BINARY = bceloss
     optimizer = optim.SGD(obj.parameters(), lr=0.001, momentum=0.9)
     epochs = 20
-    for i in range(epochs):
-        for data in iter(torch_ds_train):
-            # get the inputs; data is a list of [inputs, labels]
-            inputs, labels = data
-            optimizer.zero_grad()
-            # This doesn't work because inputs is a tuple.
-            images = list(image.to(device) for image in inputs)
-            targets = [{k: v.to(device) for k, v in t.items()} for t in labels]
-            outputs = backbone(inputs)
-            a = obj.forward(outputs['3'])
-            loss = criteria(a, labels)
-            loss.backward()
-            optimizer.step()
-        torch.save({'model': obj.state_dict(),
-                    'optimizer': optimizer.state_dict()
-                    }, str(i).zfill(3) + "resnet50_fpn_frcnn_full.tar")
+    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+    training_loop(torch_ds_train, optimizer, device, model, criteria, epochs)
 
 
-#images = list(image.to(device) for image in images)
-#            targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
+
+
+
+    #print(imgs[0][0].shape)
+    #out = model.forward(imgs[0], attrs[0])
+    #print(out)
+    #criteria = nn.CrossEntropyLoss()
+    # BINARY = bceloss
+
+    #images = list(image.to(device) for image in images)
+    #targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
 
     print('Finished Training')
 
     #trainimg_np = torch.from_numpy(trainimg).type('torch.DoubleTensor')
     #trainimg_np = torch.as_tensor(trainimg_np).type('torch.DoubleTensor')
-    print("THIS: ", trainimg.shape)
+    #print("THIS: ", trainimg.shape)
 
     #trainimg_np = trainimg_np.unsqueeze(3)
     #trainimg_np = torch.reshape(trainimg, (64, 3, 128, 1))
-    trainimg_np = trainimg.to(torch.double)
-    out = backbone(trainimg_np.float())
-    print(trainimg_np.shape)
-    print([(k, v.shape) for k, v in out.items()])
-    print(out['2'].shape)
-
-    print("\n\n\n\n\n\n\n\n\n\n\n")
-    cfg = confuse.Configuration('model_architecture', __name__, read= False)
-    cfg.set_file("C:\\Users\\Div\\Desktop\\Research\\reid\\reid\\explainable-id-reid\\src\\model_util\\classifier_architecture.yml")
-    classifier_params = cfg.get()
-    # The second argument is the output being used as a String,
-    # "1", "2", "3", or "pool"
-    obj = Classifier(classifier_params, "2")
-    a = obj.forward(out['2'])
-    print(a)
+    #trainimg_np = trainimg.to(torch.double)
+    #out = backbone(trainimg_np.float())
+    #print(trainimg_np.shape)
+    ##print([(k, v.shape) for k, v in out.items()])
+    #print(out['2'].shape)
